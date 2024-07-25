df44670bb2c947b0d41680d1594069fc410bcc21
Júlia Conifer <julia.conip@gmail.com>
2024-07-25 09:01:16 -0300

    Tweaking the UI

--- app/src/main/java/com/conifer/learningcomposable_basicscodelab/MainActivity.kt
+++ app/src/main/java/com/conifer/learningcomposable_basicscodelab/MainActivity.kt
@@ -6,11 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.*
 import com.conifer.learningcomposable_basicscodelab.ui.theme.LearningComposableBasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +32,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
